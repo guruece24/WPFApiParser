6143f1c2b657fb8a352d0533bb5ecce354c10d83
guruece24 <guruece24@gmail.com>
2020-07-17 12:09:02 +0530

    Updated User.cs

---
+++ WPFApiParser/Model/User.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WPFApiParser.Model
+{
+    public class User
+    {
+        /// <summary>
+        /// Id of user
+        /// </summary>
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        /// <summary>
+        /// Name of user
+        /// </summary>
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>
+        /// Username of User
+        /// </summary>
+        private string username;
+        public string UserName
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        /// <summary>
+        /// Email of user
+        /// </summary>
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        /// <summary>
+        /// Address Street Details
+        /// </summary>
+        private string street;
+        public string Street
+        {
+            get { return street; }
+            set { street = value; }
+        }
+
+        /// <summary>
+        /// Address Suite
+        /// </summary>
+        private string suite;
+        public string Suite
+        {
+            get { return suite; }
+            set { suite = value; }
+        }
+
+        /// <summary>
+        /// Address City
+        /// </summary>
+        private string city;
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        /// <summary>
+        /// Address Zipcode
+        /// </summary>
+        private string zipcode;
+        public string ZipCode
+        {
+            get { return zipcode; }
+            set { zipcode = value; }
+        }
+
+        /// <summary>
+        /// Latitude
+        /// </summary>
+        private string geo_lat;
+        public string Geo_Lat
+        {
+            get { return geo_lat; }
+            set { geo_lat = value; }
+        }
+
+        /// <summary>
+        /// Longitude
+        /// </summary>
+        private string geo_lang;
+        public string Geo_Lang
+        {
+            get { return geo_lang; }
+            set { geo_lang = value; }
+        }
+    }
+}
