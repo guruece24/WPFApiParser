6c5be4d95992d7bcdc8705407fbf7fd32cd8c3a3
guruece24 <guruece24@gmail.com>
2020-07-18 11:00:06 +0530

    adding extension.cs

---
+++ WPFApiParser/Model/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WPFApiParser.Model
+{
+    public static class ExtensionMethods
+    {
+        public static string RemoveQuotes(this string text)
+        {
+             return text.Replace("\"", "");             
+        }
+    }
+}
